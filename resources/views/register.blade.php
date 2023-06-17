9957c7ec532f6e405272ecc27a57b73fa8a68ca9
unknown <mutungaeric839@gmail.com>
2023-06-17 21:52:38 +0300

    styling register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,27 +1,83 @@
-<h1>Register</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <style>
+        /* Internal CSS styles for the register form */
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            flex-direction: column;
+        }
 
-@if($errors->any())
-    @foreach($errors->all() as $error)
-        <p style="color:red;">{{ $error }}</p>
-    @endforeach
-@endif
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
 
-<form action="{{ route('studentRegister') }}" method="POST">
-    @csrf
+        form {
+            text-align: center;
+        }
 
-    <input type="text" name="name" placeholder="Enter Name" required>
-    <br><br>
-    <input type="email" name="email" placeholder="Enter Email" required>
-    <br><br>
-    <input type="tel" name="phone" placeholder="Enter Phone Number" required>
-    <br><br>
-    <input type="password" name="password" placeholder="Enter Password" required>
-    <br><br>
-    <input type="password" name="password_confirmation" placeholder="Enter Confirm Password" required>
-    <br><br>
-    <input type="submit" value="Register">
-</form>
+        input[type="text"],
+        input[type="email"],
+        input[type="tel"],
+        input[type="password"] {
+            width: 300px;
+            padding: 10px;
+            margin-bottom: 10px;
+        }
 
-@if(Session::has('success'))
-    <p style="color:green;">{{ Session::get('success') }}</p>
-@endif
+        input[type="submit"] {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+
+        p {
+            color: red;
+        }
+
+        .success-message {
+            color: green;
+        }
+    </style>
+</head>
+<body>
+    <h1>Register</h1>
+
+    @if($errors->any())
+        @foreach($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    @endif
+
+    @if(Session::has('success'))
+        <p class="success-message">{{ Session::get('success') }}</p>
+    @endif
+
+    <form action="{{ route('studentRegister') }}" method="POST">
+        @csrf
+
+        <input type="text" name="name" placeholder="Enter Name" required>
+        <br><br>
+        <input type="email" name="email" placeholder="Enter Email" required>
+        <br><br>
+        <input type="tel" name="phone" placeholder="Enter Phone Number" required>
+        <br><br>
+        <input type="password" name="password" placeholder="Enter Password" required>
+        <br><br>
+        <input type="password" name="password_confirmation" placeholder="Enter Confirm Password" required>
+        <br><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
