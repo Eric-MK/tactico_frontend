d0f4b8656395ba9d1fa79aa08356257214714d34
unknown <mutungaeric839@gmail.com>
2023-06-17 22:08:40 +0300

    registration link

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,40 +1,48 @@
-<style>
-    /* Internal CSS styles for the login form and container */
-    body, html {
-        height: 100%;
-        margin: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <style>
+        /* Internal CSS styles for the login form and container */
+        body, html {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
 
-    .login-container {
-        text-align: center;
-    }
+        .login-container {
+            text-align: center;
+        }
 
-    .login-container h1 {
-        margin-bottom: 20px;
-    }
+        .login-container h1 {
+            margin-bottom: 20px;
+        }
 
-    .login-container input[type="email"],
-    .login-container input[type="password"] {
-        width: 300px;
-        padding: 10px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        margin-bottom: 10px;
-    }
+        .login-container input[type="email"],
+        .login-container input[type="password"] {
+            width: 300px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            margin-bottom: 10px;
+        }
 
-    .login-container input[type="submit"] {
-        padding: 10px 20px;
-        background-color: #4CAF50;
-        color: white;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-    }
-</style>
-
+        .login-container input[type="submit"] {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+        .login-container a {
+            text-decoration: none; /* Remove underline */
+        }
+    </style>
+</head>
+<body>
 <div class="login-container">
     <h1>Login</h1>
 
@@ -57,4 +65,8 @@
         <br><br>
         <input type="submit" value="Login">
     </form>
+
+    <p>Not a member? <a href="{{ route('studentRegister') }}">Register</a></p>
 </div>
+</body>
+</html>
