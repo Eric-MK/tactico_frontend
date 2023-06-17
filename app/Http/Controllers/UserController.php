c0b96d9f06a882ab9de602c7b798d10f959d949c
unknown <mutungaeric839@gmail.com>
2023-06-17 14:27:22 +0300

    working otp

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,11 +8,10 @@ use App\Models\EmailVerification;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
-use Mail;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
-    //
     public function loadRegister()
     {
         return view('register');
@@ -37,32 +36,32 @@ class UserController extends Controller
 
     public function loadLogin()
     {
-        if(Auth::user()){
+        if (Auth::check()) {
             return redirect('/dashboard');
         }
         return view('login');
     }
 
-    public function sendOtp($user)
+    public function sendOtp($user, $email)
     {
-        $otp = rand(100000,999999);
+        $otp = rand(100000, 999999);
         $time = time();
 
         EmailVerification::updateOrCreate(
-            ['email' => $user->email],
+            ['email' => $email],
             [
-            'email' => $user->email,
-            'otp' => $otp,
-            'created_at' => $time
+                'email' => $email,
+                'otp' => $otp,
+                'created_at' => $time
             ]
         );
 
-        $data['email'] = $user->email;
+        $data['email'] = $email;
         $data['title'] = 'Mail Verification';
+        $data['body'] = 'Your OTP is: ' . $otp;
 
-        $data['body'] = 'Your OTP is:- '.$otp;
-
-        Mail::send('mailVerification',['data'=>$data],function($message) use ($data){
+        Mail::send('mailVerification', ['data' => $data], function ($message) use ($data) {
+            $message->from('sender@example.com', 'Sender Name');
             $message->to($data['email'])->subject($data['title']);
         });
     }
@@ -74,23 +73,21 @@ class UserController extends Controller
             'password' => 'string|required'
         ]);
 
-        $userCredential = $request->only('email','password');
-        $userData = User::where('email',$request->email)->first();
-        if($userData && $userData->is_verified == 0){
-            $this->sendOtp($userData);
-            return redirect("/verification/".$userData->id);
-        }
-        else if(Auth::attempt($userCredential)){
+        $userCredential = $request->only('email', 'password');
+        $userData = User::where('email', $request->email)->first();
+        if ($userData && $userData->is_verified == 0) {
+            $this->sendOtp($userData, $request->email);
+            return redirect("/verification/" . $userData->id);
+        } elseif (Auth::attempt($userCredential)) {
             return redirect('/dashboard');
-        }
-        else{
-            return back()->with('error','Username & Password is incorrect');
+        } else {
+            return back()->with('error', 'Username & Password is incorrect');
         }
     }
 
     public function loadDashboard()
     {
-        if(Auth::user()){
+        if (Auth::check()) {
             return view('dashboard');
         }
         return redirect('/');
@@ -98,58 +95,54 @@ class UserController extends Controller
 
     public function verification($id)
     {
-        $user = User::where('id',$id)->first();
-        if(!$user || $user->is_verified == 1){
+        $user = User::where('id', $id)->first();
+        if (!$user || $user->is_verified == 1) {
             return redirect('/');
         }
         $email = $user->email;
 
-        $this->sendOtp($user);//OTP SEND
+        $this->sendOtp($user, $email); //OTP SEND
 
-        return view('verification',compact('email'));
+        return view('verification', compact('email'));
     }
 
     public function verifiedOtp(Request $request)
     {
-        $user = User::where('email',$request->email)->first();
-        $otpData = EmailVerification::where('otp',$request->otp)->first();
-        if(!$otpData){
-            return response()->json(['success' => false,'msg'=> 'You entered wrong OTP']);
+        $user = User::where('email', $request->email)->first();
+        $otpData = EmailVerification::where('otp', $request->otp)->first();
+        if (!$otpData) {
+            return response()->json(['success' => false, 'msg' => 'You entered the wrong OTP']);
         }
-        else{
+        else {
+                $currentTime = time();
+                $time = $otpData->created_at;
+
+                if ($currentTime >= $time && $time >= $currentTime - (90 + 5)) { // 90 seconds
+                    User::where('id', $user->id)->update([
+                        'is_verified' => 1
+                    ]);
+                    return response()->json(['success' => true, 'msg' => 'Mail has been verified']);
+                } else {
+                    return response()->json(['success' => false, 'msg' => 'Your OTP has expired']);
+                }
+            }
+        }
+
+        public function resendOtp(Request $request)
+        {
+            $user = User::where('email', $request->email)->first();
+            $otpData = EmailVerification::where('email', $request->email)->first();
 
             $currentTime = time();
             $time = $otpData->created_at;
 
-            if($currentTime >= $time && $time >= $currentTime - (90+5)){//90 seconds
-                User::where('id',$user->id)->update([
-                    'is_verified' => 1
-                ]);
-                return response()->json(['success' => true,'msg'=> 'Mail has been verified']);
+            if ($currentTime >= $time && $time >= $currentTime - (90 + 5)) { // 90 seconds
+                return response()->json(['success' => false, 'msg' => 'Please try again after some time']);
+            } else {
+                $this->sendOtp($user, $request->email); //OTP SEND
+                return response()->json(['success' => true, 'msg' => 'OTP has been sent']);
             }
-            else{
-                return response()->json(['success' => false,'msg'=> 'Your OTP has been Expired']);
-            }
-
         }
     }
 
-    public function resendOtp(Request $request)
-    {
-        $user = User::where('email',$request->email)->first();
-        $otpData = EmailVerification::where('email',$request->email)->first();
 
-        $currentTime = time();
-        $time = $otpData->created_at;
-
-        if($currentTime >= $time && $time >= $currentTime - (90+5)){//90 seconds
-            return response()->json(['success' => false,'msg'=> 'Please try after some time']);
-        }
-        else{
-
-            $this->sendOtp($user);//OTP SEND
-            return response()->json(['success' => true,'msg'=> 'OTP has been sent']);
-        }
-
-    }
-}
