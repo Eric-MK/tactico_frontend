9f2137cedbfe3d19e29e3395bfcd2a0f532ba9ab
unknown <mutungaeric839@gmail.com>
2023-07-09 14:19:26 +0300

    footer

--- resources/views/frontend/FooterPage.blade.php
+++ resources/views/frontend/FooterPage.blade.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <style>
         .main-footer {
-            box-shadow: 0 -5px 15px rgba(0,0,0,0.1);
+            box-shadow: 0 -5px 15px rgba(10, 10, 10, 0.1);
         }
     </style>
 </head>
