a3052f4c81f54589cc65c27881d53c5f242bb7f4
unknown <mutungaeric839@gmail.com>
2023-07-09 12:34:47 +0300

    footer code and update

--- resources/views/frontend/FooterPage.blade.php
+++ resources/views/frontend/FooterPage.blade.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+        .main-footer {
+            box-shadow: 0 -5px 15px rgba(0,0,0,0.1);
+        }
+    </style>
 </head>
 <body>
     <footer class="main-footer footer mt-auto bg-light py-3">
