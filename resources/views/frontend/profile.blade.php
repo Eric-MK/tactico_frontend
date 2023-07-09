9175b5d61659188df9b29aa30c635b9882736d45
unknown <mutungaeric839@gmail.com>
2023-07-09 17:59:47 +0300

    use of SweetAlert2 library

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <style>
         .main-body {
@@ -104,13 +105,22 @@
         }
 
         // Confirm submission
-        if (!confirm(message)) {
-            // If user clicks "Cancel", prevent form submission
-            e.preventDefault();
-        }
+        e.preventDefault(); // Always prevent the default first
+
+        swal({
+            title: "Confirm Submission",
+            text: message,
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, I'm sure!",
+            closeOnConfirm: false
+        }, function() {
+            e.target.submit(); // Only submit the form in the callback when the user has confirmed
+        });
     });
 </script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
