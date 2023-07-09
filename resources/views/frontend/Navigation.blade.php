aa15426afaa10d2e078751b47a4842690fb6b5c4
unknown <mutungaeric839@gmail.com>
2023-07-09 10:47:18 +0300

    styling navigation

--- resources/views/frontend/Navigation.blade.php
+++ resources/views/frontend/Navigation.blade.php
@@ -4,20 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+       .nav-link {
+    text-decoration: none;
+}
+
+.nav-link.active {
+    border: 1px solid black;
+    border-radius: 9px;
+    text-decoration: none;
+}
+
+.nav-link:hover {
+    text-decoration: underline;
+}
+
+        .logout{
+           color: red;
+           text-decoration: none;
+
+        }
+
+
+    </style>
 </head>
 <body>
 <!-- Your content here -->
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Tactico</a>
+        <a class="navbar-brand" href="/dashboard">Tactico</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link active" href="/dashboard">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Shortlist</a>
@@ -34,7 +57,7 @@
             </span>
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                    <a class="nav-link logout" href="{{ route('logout') }}">Logout</a>
                 </li>
             </ul>
         </div>
